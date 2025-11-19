5f7d552744f3b40aa426f0bd791452ef2eea1987
ju <beau@insa-toulouse.fr>
2025-11-19 11:37:34 +0100

    implement clone_nodes, gmap and add_arc in tools [not tested]

---
+++ src/tools.ml
@@ -0,0 +1,21 @@
+open Graph
+
+(* assert false is of type ∀α.α, so the type-checker is happy. *)
+(* 
+let clone_nodes _gr = assert false
+let gmap _gr _f = assert false
+*)
+
+
+
+let clone_nodes (gr : 'a graph) =
+  n_fold gr (fun newg id -> new_node newg id) empty_graph
+
+let gmap gr f = 
+  e_fold gr (fun newg a -> new_arc newg {src=a.src; tgt= a.tgt; lbl=f a.lbl}) (clone_nodes gr)
+
+let add_arc g id1 id2 n =
+  let a = find_arc g id1 id2 in
+  match a with 
+  |None -> new_arc g {src=id1;tgt=id2;lbl=n}
+  |Some ar -> new_arc g {src=ar.src; tgt=ar.tgt; lbl=(ar.lbl+n) }
