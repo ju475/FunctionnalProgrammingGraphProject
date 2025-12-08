c3a7693d3adbd5280f123627d9a752d2871639e4
ju <beau@insa-toulouse.fr>
2025-12-08 16:31:45 +0100

    :poop: starting FF algo

--- src/ford_fulkerson.ml
+++ src/ford_fulkerson.ml
@@ -19,16 +19,26 @@ in
     e_fold fg add_both (clone_nodes fg) 
 
 
-(*
+
 let ford_fulkerson (cg:cap_graph) (srcNode:id) (tgtNode:id)  =
-if not (node_exists eg srcNode) then raise (Graph_error "Source Node Not Exists")
-    else if not (node_exists eg tgtNode) then raise (Graph_error "Target Node Not Exists")
+if not (node_exists cg srcNode) then raise (Graph_error "Source Node Not Exists")
+    else if not (node_exists cg tgtNode) then raise (Graph_error "Target Node Not Exists")
+    else let fg = cap2flot cg in 
+    let rec loop () =
+        let eg = flot2ecart fg in
+        let chemin = journey eg srcNode tgtNode in
+        let delta = flotmin chemin in  
+        let deal_delta fg a = if a.lbl > 0 then new_arc fg {src=a.src; tgt=a.tgt; lbl=a.lbl+delta} 
+            else new_arc fg {src=a.tgt; tgt= a.src; lbl= a.lbl-delta} in 
+
+            let update_flot = e_fold eg deal_delta fg
+    in loop ()
 
 
 let rec journey (eg:ecart_graph) (srcNode:id) (tgtNode:id) =
     (* we need to add a accu for the visited nodes and do a dfs*)
 
-*)
+
 
 let flotmin (jrn:(int arc) list) =
     List.fold_left (fun acc new_val -> if (new_val.lbl < acc) then new_val.lbl else acc) (int_of_float infinity) jrn 
